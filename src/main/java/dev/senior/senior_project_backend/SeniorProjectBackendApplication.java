d5632a8d8bdc96233d2f1a868d1202e315855025
Karassay <karassay.raushanbek@gmail.com>
2024-11-16 17:24:30 +0500

    init

---
+++ src/main/java/dev/senior/senior_project_backend/SeniorProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.senior.senior_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/senior/senior_project_backend/SeniorProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.senior.senior_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
